fc867ce3ebebea394a6d8dbfd57ce42702ad1b1d
plusbeauxjours <plusbeauxjours@gmail.com>
2025-04-01 17:47:57 +0900

    WIP: shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,5 +1,6 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -9,5 +10,9 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <h1>
+      home <Button>Click me</Button>
+    </h1>
+  );
 }
