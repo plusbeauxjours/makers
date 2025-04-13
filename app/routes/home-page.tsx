44bb381421f7ebf2de58cffbbfe7cf486b3bae9d
plusbeauxjours <plusbeauxjours@gmail.com>
2025-04-13 17:03:50 +0900

    set: Post Card

--- app/routes/home-page.tsx
+++ app/routes/home-page.tsx
@@ -1,4 +1,6 @@
-import type { MetaFunction } from "react-router";
+import { Link, type MetaFunction } from "react-router";
+import { Button } from "~/common/components/ui/button";
+import { PostCard } from "~/features/products/components/post-card";
 import { ProductCard } from "~/features/products/components/product-card";
 
 export const meta: MetaFunction = () => {
@@ -7,15 +9,18 @@ export const meta: MetaFunction = () => {
 
 export default function HomePage() {
   return (
-    <div className="px-20">
+    <div className="px-20 space-y-40">
       <div className="grid grid-cols-3 gap-4">
         <div>
           <h2 className="text-5xl font-bold leading-tight tracking-tight">Today's Products</h2>
           <p className="text-xl font-light text-foreground">
             The best products made by our community today.
           </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/products/leaderboards">Explore all products &rarr;</Link>
+          </Button>
         </div>
-        {Array.from({ length: 10 }).map((_, index) => (
+        {Array.from({ length: 11 }).map((_, index) => (
           <ProductCard
             id={`productId-${index}`}
             name="Product Name"
@@ -23,6 +28,27 @@ export default function HomePage() {
             commentsCount={12}
             viewsCount={12}
             votesCount={120}
+          />
+        ))}
+      </div>
+      <div className="grid grid-cols-3 gap-4">
+        <div>
+          <h2 className="text-5xl font-bold leading-tight tracking-tight">Latest Discussions</h2>
+          <p className="text-xl font-light text-foreground">
+            The latest discussions from our community.
+          </p>
+          <Button variant="link" asChild className="text-lg p-0">
+            <Link to="/community">Explore all discussions &rarr;</Link>
+          </Button>
+        </div>
+        {Array.from({ length: 11 }).map((_, index) => (
+          <PostCard
+            id={`postId-${index}`}
+            title="What is the best productivity tool?"
+            author="Nico"
+            authorAvatarUrl="https://github.com/apple.png"
+            category="Productivity"
+            postedAt="12 hours ago"
           />
         ))}
       </div>
