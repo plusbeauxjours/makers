090ac0b50b806023df7894f82972a92b46e2a982
plusbeauxjours <plusbeauxjours@gmail.com>
2025-04-14 23:40:38 +0900

    remove: potato

--- app/potato/tomato.tsx
+++
@@ -1,3 +0,0 @@
-export function Tomato() {
-  return <div>Tomato</div>;
-}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,25 @@
-import { type RouteConfig, index } from "@react-router/dev/routes";
+import { type RouteConfig, index, prefix, route } from "@react-router/dev/routes";
 
-export default [index("routes/home-page.tsx")] satisfies RouteConfig;
+export default [
+  index("routes/home-page.tsx"),
+  ...prefix("products", [
+    index("features/products/pages/products-page.tsx"),
+    ...prefix("leaderboards", [
+      index("features/products/pages/leaderboards-page.tsx"),
+      route("/yearly/:year", "features/products/pages/yearly-leaderboards-page.tsx"),
+      route("/monthly/:year/:month", "features/products/pages/monthly-leaderboards-page.tsx"),
+      route("/weekly/:year/:week", "features/products/pages/weekly-leaderboards-page.tsx"),
+      route("/daily/:year/:month/:day", "features/products/pages/daily-leaderboards-page.tsx"),
+    ]),
+    ...prefix("categories", [
+      index("features/products/pages/categories-page.tsx"),
+      route("/:category", "features/products/pages/category-page.tsx"),
+    ]),
+    ...prefix("search", [
+      index("features/products/pages/search-page.tsx"),
+      route("/:query", "features/products/pages/search-results-page.tsx"),
+    ]),
+    route("/submit", "features/products/pages/submit-page.tsx"),
+    route("/promote", "features/products/pages/promote-page.tsx"),
+  ]),
+] satisfies RouteConfig;
