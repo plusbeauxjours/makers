4df74c8c1033030b323fda60b581a4e13025cd5d
plusbeauxjours <plusbeauxjours@gmail.com>
2025-04-15 17:49:32 +0900

    set: Daily Leaderboard

---
+++ app/common/components/product-pagination.tsx
@@ -0,0 +1,7 @@
+type ProductPaginationProps = {
+  totalPages: number;
+};
+
+export default function ProductPagination({ totalPages }: ProductPaginationProps) {
+  return <div>ProductPagination</div>;
+}
