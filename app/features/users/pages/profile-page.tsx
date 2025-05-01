0ab6d8599b6d88471237a86ee4b3ba96451491e4
plusbeauxjours <plusbeauxjours@gmail.com>
2025-05-01 23:23:13 +0900

    ProfilePage

--- app/features/users/pages/profile-page.tsx
+++ app/features/users/pages/profile-page.tsx
@@ -6,9 +6,21 @@ export const meta: Route.MetaFunction = () => {
 
 export default function ProfilePage() {
   return (
-    <div className="container py-8">
-      <h1 className="text-2xl font-semibold mb-6">Profile</h1>
-      <div className="grid gap-6">{/* Profile content will go here */}</div>
+    <div className="max-w-screen-md flex flex-col space-y-10">
+      <div className="space-y-2">
+        <h4 className="text-lg font-bold">Headline</h4>
+        <p className="text-muted-foreground">
+          I'm a product designer based on the UK, I like doing product design, design systems and
+          design tokens.
+        </p>
+      </div>
+      <div className="space-y-2">
+        <h4 className="text-lg font-bold">About</h4>
+        <p className="text-muted-foreground">
+          I'm a product designer based on the UK, I like doing product design, design systems and
+          design tokens.
+        </p>
+      </div>
     </div>
   );
 }
