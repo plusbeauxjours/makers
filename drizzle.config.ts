2564453a2f6d427dae7ce484220dedf0efbbe457
plusbeauxjours <plusbeauxjours@gmail.com>
2025-05-09 01:15:46 +0900

    Migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-    schemaFilter: ['public'],
     schema: './app/features/**/schema.ts',
     out: './app/migrations',
     dialect: 'postgresql',
